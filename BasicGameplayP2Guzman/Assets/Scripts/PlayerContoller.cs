dee513b38e19f248ed933976524a301ae4dad43c
emilygg1217 <emilyg1217@icloud.com>
2025-03-24 10:21:04 -0700

    x
    xd

--- BasicGameplayP2Guzman/Assets/Scripts/PlayerContoller.cs
+++ BasicGameplayP2Guzman/Assets/Scripts/PlayerContoller.cs
@@ -13,8 +13,24 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
 
     public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
     void Update()
-    {
+        // Keep the player in bounds
+    { if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+
+
     }
 }
